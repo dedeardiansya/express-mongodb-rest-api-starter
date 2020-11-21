1274f58b0dfce41f3dc199469179d0c3d58235ea
dede ardiansya <dedeariansya1@gmail.com>
2020-11-21 23:28:29 +0800

    handle error

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ import compression from 'compression'
 import cors from 'cors'
 import mongoSanitize from 'express-mongo-sanitize'
 import cookieParser from 'cookie-parser'
+import httpStatus from 'http-status'
 import morgan from './config/morgan'
+import ApiError from './utils/ApiError'
+import { errorConverter, errorHandler } from './middlewares/error'
 
 const app = express()
 
@@ -20,5 +23,11 @@ app.use(compression())
 app.use(cors())
 app.options('*', cors())
 app.use(cookieParser())
+
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'))
+})
+app.use(errorConverter)
+app.use(errorHandler)
 
 export default app
