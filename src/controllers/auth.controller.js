5aea54f099cb147901f9a2997573d2ef87973a71
dede ardiansya <dedeariansya1@gmail.com>
2020-11-22 04:19:28 +0800

    lint fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import httpStatus from 'http-status'
 import catchAsync from '../utils/catchAsync'
 import { userService, tokenService, authService, emailService } from '../services'
 
-export const register = catchAsync(async (req, res) => {
+const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body)
   const tokens = await tokenService.generateAuthTokens(user)
   res.status(httpStatus.CREATED).send({ user, tokens })
